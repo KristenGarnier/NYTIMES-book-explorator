55f41cbb99f1c4644e31a62e4d16088baba824ba
KristenGarnier <garnier.kristen@icloud.com>
2016-02-02 17:34:14 +0100

    Creating a crossplatform component

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import React, { AppRegistry } from 'react-native';
-import Explore from './explore';
+import Explore from './components/crossPlatform';
 
 AppRegistry.registerComponent('exploringnative', () => Explore);
